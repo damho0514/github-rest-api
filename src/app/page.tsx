5f2803e169ce69861233ab7a01c101aed433436a
damhobae <damho0514@gmail.com>
2024-10-27 21:56:49 +0900

    feat: 리스트 카드 컴포넌트 및 목데이터를 추가합니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
-import Image from "next/image";
 import React from "react";
+import UserList from "./_components/UserList";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex  flex-col items-center justify-between p-24">
+      <div
+        style={{ height: "100px" }}
+        className="font-bold text-2xl items-center flex"
+      >
+        User List
+      </div>
+      <UserList />
+    </main>
   );
 }
